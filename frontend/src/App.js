37ca632d6f9aa1eac20651427d255765c5d17084
Adel NOUAR <adel.nouar@protonmail.com>
2021-08-19 20:35:43 +0200

    Save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -18,7 +20,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
