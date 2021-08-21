0fc4363ddaac78e05d8381d45a57cdc31e3053e3
Adel NOUAR <adel.nouar@protonmail.com>
2021-08-21 23:16:30 +0200

    Adjust layout of the image cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // unsplash key stored in a gitignore file for not sharing it on github
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
@@ -32,9 +33,15 @@ const App = () => {
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      {images.map((image, i) => (
-        <ImageCard key={i} image={image} />
-      ))}
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
