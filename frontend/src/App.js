eae06713c9d729af025bc3438efc744be52f24a4
Adel NOUAR <adel.nouar@protonmail.com>
2021-08-22 11:07:27 +0200

    Display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,6 @@ const App = () => {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
@@ -29,6 +28,10 @@ const App = () => {
     setWord('');
   };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
   return (
     <div>
       <Header title="Images Gallery" />
@@ -37,7 +40,7 @@ const App = () => {
         <Row xs={1} md={2} lg={3}>
           {images.map((image, i) => (
             <Col key={i} className="pb-3">
-              <ImageCard image={image} />
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
             </Col>
           ))}
         </Row>
